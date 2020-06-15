4c2cd856087615342e357a3c42c275a068a6401b
Alejandro Fajardo <alejandro@progresus.co>
2020-06-15 14:51:27 -0500

    Query Profiles

--- gam/sections/users/profiles.php
+++ gam/sections/users/profiles.php
@@ -5,7 +5,7 @@ if($_POST['action']=='create')
 	$campos  = "name,description";
 	$valores = "'$_POST[name]','$_POST[description]'";
 	$alert=true;
-	if(add("profiles",$campos,$valores,1)){
+	if(add("profiles",$campos,$valores,0)){
 		$type="success";
 		$message = "Registro guardado correctamente";
 	}
@@ -136,3 +136,81 @@ if($alert)
      </div>
    </div>
  </div>
+
+<!---------------------------------------------------------------- LISTADO ----------------------------------------------------->
+<?php
+//--------------------Query-------------------
+$id="id_profile";
+$cols="*";
+$table="profiles";
+$where="1 ORDER BY name ASC";
+$result=query($table,$cols,$where,false);
+?>
+<div class="box box-warning">
+   <div class="box-header with-border">     
+     <h3 style="margin:5px !important">
+       <i class="fa fa-list"></i>  Perfiles
+     </h3>
+     <div class="box-tools pull-right">
+       <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+       <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-remove"></i></button>
+     </div>
+   </div>
+   <div class="box-body"> 
+   <br><br> 
+    <div style="overflow-x:scroll">
+	    <form name="forma1" method="post" action="">
+	     <table id="generalDataTable" class="table table-bordered table-striped">
+	       <thead>
+	         <tr>
+				<td class="text-center"><b>Nombre</b></td>
+				<td class="text-center"><b>Descripción</b></td>
+				<td class="text-center"><b>Acciones</b></td>
+			 </tr>
+	       </thead>
+	       <tbody>
+		   <?php
+			   if($result)
+			   {
+					foreach ($result as $res) {
+					?>
+						<tr>
+							<td class="text-center"><?=$res["name"]?></td>
+							<td class="text-center"><?=$res["description"]?></td>
+							<td class="text-center">
+								<a href="?section=users&module=admin_user&item=<?=$res["id_usuario"]?>" >
+									<input type="button" value="Editar" class="btn btn-primary btn-xs" />									
+								</a>
+								<?php
+								if($res["estado"]==0)
+								{
+								?>
+									<input type="button" name="activar" id="activar" value="Activar" class="btn btn-success btn-xs" onclick="asignarAccion1('activar',<?=$res['id_trivia']?>)"/> 
+								<?php	
+								}
+								else
+								{
+								?>
+									<input type="button" name="desactivar" id="desactivar" value="Desactivar" class="btn btn-danger btn-xs" onclick="asignarAccion1('desactivar',<?=$res['id_trivia']?>)" />
+								<?php	
+								}
+								?>																	
+							</td>
+						</tr>
+					<?php
+					}
+			   }
+		   ?>
+	       </tbody>
+	     </table>
+	     <input type="hidden" id="accionEdit" name="accionEdit" />
+	     <input type="hidden" id="filtro" name="filtro" />
+		 <input type="hidden" id="idElemento" name="idElemento" />
+		</form><!-- Form -->
+		<br>
+	</div><!-- Scroll -->
+   </div>
+</div> <!-- Listado -->
+ 
+</div><!-- Contenedor general -->
+<br>
